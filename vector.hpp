9ae54afb224e757e78cc36c1fa9b0815e1d8499d
Patryk Ząbkiewicz <pzabkiewicz@gmail.com>
2015-02-06 12:39:03 +0100

    features added

---
+++ vector.hpp
@@ -0,0 +1,20 @@
+
+/*
+ 
+ Licensed under LGPL3 license.
+ 
+ If you need a support I am providing support-as-service for the library.
+ 
+ To contact me, please write an email to pzabkiewicz@gmail.com
+ 
+*/
+
+typedef <class T>
+struct vec : public vector<T> {
+
+	operator() const {
+		
+	}
+
+};
+
